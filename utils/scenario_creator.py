7fcf0abcf85a7f976ffc47b80d865a5742db2d6a
nicolasmr982 <nmerchanr@unal.edu.co>
2025-01-22 22:21:19 -0500

    Resultados santa rosalia

--- utils/scenario_creator.py
+++ utils/scenario_creator.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+from datetime import datetime
+import locale
+import os
+locale.setlocale(locale.LC_ALL, 'es_ES')
 
 def create_weather_BMG_scens(df, pdf_scipy, col, seed = None):
 
@@ -64,44 +68,111 @@ def create_weather_BMG_scens(df, pdf_scipy, col, seed = None):
     
     return df_scens, scens_prob
 
-def plot_pdf(df,pdf,col_data,month = 1,units=""):
-    matplotlib.rcParams.update({'font.size': 15})
+def plot_pdf(
+        df: pd.DataFrame, 
+        pdf, 
+        month: int = 1,
+        hour: int = 1,
+        col_data: str = "", 
+        name_var: str = "",
+        units: str = "", 
+        location=None
+    ):
+    """
+    Genera una figura combinada con:
+    - Dos subplots en la izquierda (verticales) para los gráficos de la columna seleccionada "col_data".
+    - Una cuadrícula de subplots en la derecha para PDFs por hora.
+    """
+    matplotlib.rcParams.update({'font.size': 16})   
 
-    fig, ax = plt.subplots(4,6, figsize=(40,28))
+    font_size = 22
 
-    hour = 0
-    for row in range(0,4):
-        for col in range(0,6):
-            data = df.loc[np.logical_and(df.Hour == hour, df.Month == month) ,col_data].to_numpy()
-            
-            ax[row,col].hist(data, bins=40,density=True)
-            
-            params = pdf.fit(data, loc = 0)
-            dist = pdf(*params)  
-            
+    month_name = datetime(1900, month, 1).strftime('%B')
+    years = [2015, 2016, 2017, 2018, 2019, 2020]
 
-            x = np.linspace(dist.ppf(0.01),
-                    dist.ppf(0.9999),10000)
+    # Crear figura principal
+    fig = plt.figure(figsize=(40, 25))  # Tamaño ajustado para integrar todos los gráficos
+
+    # Subplot Izquierdo Superior: de la columna seleccionada "col_data" por mes
+    ax1 = plt.subplot2grid((4,9), (0, 0), rowspan=2, colspan=3)
+    for year in years:
+        mask = np.logical_and(df["Year"] == year, df["Month"] == month)
+        x = df.index[mask]
+        y = df.loc[mask, col_data].to_numpy()
+        ax1.plot(x, y, label=f'{year}')
+    ax1.set_title(f'{name_var} en {location["name"].capitalize()}. {month_name}',fontsize=font_size, fontweight='bold')
+    ax1.set_ylabel(f'${units}$',fontsize=font_size)
+    ax1.legend(fontsize=font_size)
+    ax1.tick_params(axis='x', labelsize=font_size) 
+    ax1.tick_params(axis='y', labelsize=font_size) 
+    ax1.grid()
+
+    # Subplot Izquierdo Inferior: de la columna seleccionada "col_data" a una hora específica
+    ax2 = plt.subplot2grid((4,9), (2, 0), rowspan=2, colspan=3)
+    for year in years:
+        mask = np.logical_and.reduce([df["Year"] == year, df["Month"] == month, df["Hour"] == hour])
+        x = df.index[mask]
+        y = df.loc[mask, col_data].to_numpy()
+        ax2.scatter(x, y, label=f'{year}', alpha=0.7)
+    ax2.set_title(f'{name_var} en {location["name"].capitalize()}. {month_name} a las {hour}:00',fontsize=font_size, fontweight='bold')
+    ax2.set_xlabel("Fecha",fontsize=font_size)
+    ax2.set_ylabel(f'${units}$',fontsize=font_size)
+    ax2.legend(fontsize=font_size)
+    ax2.tick_params(axis='x', labelsize=font_size) 
+    ax2.tick_params(axis='y', labelsize=font_size) 
+    ax2.grid()
+
+    for spine in ax2.spines.values():  # ax2 es el eje al que deseas resaltar
+        spine.set_edgecolor('red')  # Color del recuadro
+        spine.set_linewidth(4)  # Grosor del recuadro
+
+    # Subplot Derecho: Figura de PDFs
+    hour_tmp = 0
+    for row in range(0, 4):
+        for col in range(0, 6):  # Usar 3 columnas para los subplots de la derecha
+            ax_pdf = plt.subplot2grid((4,9), (row, col + 3))  # Inicia en la cuarta columna
+            data = df.loc[np.logical_and(df.Hour == hour_tmp, df.Month == month), col_data].to_numpy()
+
+            # Mostrar el histograma y la PDF
+            ax_pdf.hist(data, bins=40, density=True)
+
+            params = pdf.fit(data, loc=0)
+            dist = pdf(*params)
+
+            x = np.linspace(dist.ppf(0.01), dist.ppf(0.9999), 10000)
             y1 = dist.pdf(x)
 
             mean, std = dist.mean(), dist.std()
+
+            ax_pdf.plot(x, y1, linewidth=2.5, label="pdf")
+            ax_pdf.axvline(x=mean, linewidth=2.5, color='#F75D3C', label='media')
+            ax_pdf.axvline(x=mean + std, linewidth=2.5, color='#58F73C', label='media + std')
+            ax_pdf.axvline(x=mean - std, linewidth=2.5, color='#F73CF7', label='media - std')
+
+            if hour_tmp == hour:
+                for spine in ax_pdf.spines.values():  
+                    spine.set_edgecolor('red') 
+                    spine.set_linewidth(4)  
+
+
+            ax_pdf.set_title(f'Hora {hour_tmp}:00',fontsize=font_size, fontweight='bold')
+            ax_pdf.tick_params(axis='x', labelsize=font_size) 
+            ax_pdf.tick_params(axis='y', labelsize=font_size) 
+            ax_pdf.grid()
+            ax_pdf.legend(fontsize=16)
+            ax_pdf.set_xlabel(f'${units}$', loc='right',fontsize=font_size, labelpad=0)
+
+             
             
-            ax[row,col].plot(x,y1, linewidth=3.5, label = "pdf")
-            ax[row,col].axvline(x = mean, linewidth=3.5, color = '#F75D3C', label = 'mean')
-            ax[row,col].axvline(x = mean + std, linewidth=3.5, color = '#58F73C', label = 'mean + std')
-            ax[row,col].axvline(x = mean - std, linewidth=3.5, color = '#F73CF7', label = 'mean - std')
-            
-            ax[row,col].set_title(f'Hora {hour}:00')
-            ax[row,col].grid()
-            ax[row,col].legend()
-            
-            x1 = np.linspace(mean + std, x[-1] , 100)
-            #ax[row,col].fill_between(x1, 0, dist.pdf(x1), color = 'r')
-            ax[row,col].set_xlabel(units)
-            hour += 1
+            hour_tmp += 1
+
+    path = "Figuras/pdf"
+    os.makedirs(path, exist_ok=True) 
+
+    plt.savefig(f'{path}/pdf_{location["name"]}_{col_data}.pdf', format='pdf', transparent=True)
 
     return fig
-
+    
 def create_av_scens(data, seed = None):
 
     if seed is not None:
